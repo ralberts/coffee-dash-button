2373052ba20edc3956a56c2baaa029a372141622
Ryan Alberts <ryan@ryanalberts.com>
2017-05-31 21:03:28 -0600

    Adding ability to trigger without notifications without the use of the REST API.

--- dashButton/index.js
+++ dashButton/index.js
@@ -1,27 +1,54 @@
 import DashButton from 'dash-button';
-var http = require('http');
-var request = require('request');
-const BUTTONS = require('./constants').BUTTONS;
-const DASH_BUTTON_MAC_ADDRESS = BUTTONS[0].mac;
+const http = require('http');
+const request = require('request');
+const {BUTTONS, REST_API, REST_API_URL} = require('./constants');
 
-let button = new DashButton(DASH_BUTTON_MAC_ADDRESS);
+var CoffeePot = require('../coffeeBrewNotify/coffee');
+var HipChat = require('../coffeeBrewNotify/hipchat');
+var hipChat = new HipChat();
+var coffee = new CoffeePot();
 
-let subscription = button.addListener(async () => {
-	console.log("Button pressed!");
-	request({
-		url: 'http://ryanalberts.com/coffee/index.php',
-		method: 'POST',
-		json: true,
-		body: {
-			action: 'brewing',
-			time: new Date().toISOString()
-		}
-	}, function (error, response, body) {
-		console.log(response, body, error);
-	});
-	
+const DASH_BUTTON_MAC_ADDRESS_1 = BUTTONS[0].mac;
+
+BUTTONS.forEach((buttonConfig) => {
+	let button = new DashButton(buttonConfig.mac);
+	execute(button, buttonConfig);
 });
 
+function execute(button, config) {
+	if(REST_API) {
+		let subscription = button.addListener(async () => {
+			console.log("Button pressed!");
+			request({
+				url: REST_API_URL,
+				method: 'POST',
+				json: true,
+				body: {
+					action: 'brewing',
+					time: new Date().toISOString()
+				}
+			}, function (error, response, body) {
+				console.log(response, body, error);
+			});
+		});
+	} else {
+		let subscription = button.addListener(async () => {
+			console.log("Button pressed! Coffee type: ", config.coffeeType);
+
+			if(coffee.alreadyBrewing()) {
+				console.log("Already brewing some coffee");
+				return;
+			}
+
+			coffee.registerCallback(function() {
+				const msg = config.coffeeType + " coffee finished brewing. Please drink responsibly.";
+				//hipChat.notifyRoom(msg);
+			});
+		
+			coffee.brew(new Date().toISOString());
+		});
+	}
+}
 console.log("All ready!");
 
 
